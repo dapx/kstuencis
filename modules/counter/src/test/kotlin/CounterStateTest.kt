d8a394dc335abc8de59bc2de06ac00e455de4ac8
Daniel André da Silva <dap1995@gmail.com>
2021-03-12 19:50:28 -0300

    Test decrement event when dispatching decrement event

--- modules/counter/src/test/kotlin/CounterStateTest.kt
+++ modules/counter/src/test/kotlin/CounterStateTest.kt
@@ -2,18 +2,22 @@ package kstuencis.counter
 
 import kotlinx.coroutines.runBlocking
 import kstuencis.counter.CounterEvent.Increment
+import kstuencis.counter.CounterEvent.Decrement
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class CounterStateTest {
     @Test
-    fun `Should change the state on dispatch`() = runBlocking {
+    fun `Should change the state when dispatching event`() = runBlocking {
         val counter = CounterState()
 
-        counter.dispatch(Increment(payload = 5))
+        counter.dispatch(Increment(5))
         assertEquals(expected = 5, actual = counter.compute())
 
-        counter.dispatch(Increment(payload = 3))
-        assertEquals(expected = 8, actual = counter.compute())
+        counter.dispatch(Decrement(2))
+        assertEquals(expected = 3, actual = counter.compute())
+
+        counter.dispatch(Increment(3))
+        assertEquals(expected = 6, actual = counter.compute())
     }
 }
