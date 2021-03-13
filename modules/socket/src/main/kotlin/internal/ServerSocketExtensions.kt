0eff6297459c404766b48b40ab8d0dde8b03d0f6
Daniel André da Silva <dap1995@gmail.com>
2021-03-13 13:12:49 -0300

    Handle socket close exception when accepting connections

--- modules/socket/src/main/kotlin/internal/ServerSocketExtensions.kt
+++ modules/socket/src/main/kotlin/internal/ServerSocketExtensions.kt
@@ -4,14 +4,28 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.ServerSocket
 import java.net.Socket
+import java.net.SocketException
 
-internal suspend fun ServerSocket.acceptAndExecute(block: suspend Socket.() -> Unit) = withContext(Dispatchers.IO) {
+internal suspend fun ServerSocket.waitForConnectionAndExecute(
+    block: suspend Socket.() -> Unit
+) = withContext(Dispatchers.IO) {
     use { server ->
-        while (true) try {
-            val socket = server.accept()
-            socket.block()
+        while (!isClosed) try {
+            val socket = server.acceptNotThrowingWhenUnexpectedClosed()
+            socket?.block()
         } catch (ex: Exception) {
             ex.printStackTrace()
+        }
+    }
+}
+
+private suspend fun ServerSocket.acceptNotThrowingWhenUnexpectedClosed(): Socket? = withContext(Dispatchers.IO) {
+    try {
+        return@withContext accept()
+    } catch (ex: SocketException) {
+        when (isClosed) {
+            false -> throw ex
+            else -> null
         }
     }
 }
