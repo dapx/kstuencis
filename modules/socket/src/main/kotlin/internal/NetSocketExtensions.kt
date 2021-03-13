c19198d10f0918c6b6df9b60855b15e8e81748ea
Daniel André da Silva <dap1995@gmail.com>
2021-03-13 13:16:44 -0300

    Do not close the socket when reading or writing

--- modules/socket/src/main/kotlin/internal/NetSocketExtensions.kt
+++ modules/socket/src/main/kotlin/internal/NetSocketExtensions.kt
@@ -2,6 +2,7 @@ package kstuencis.socket.internal
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.net.SocketException
 import java.net.Socket as NetSocket
 
 internal suspend fun NetSocket.listenMessages(
@@ -11,16 +12,24 @@ internal suspend fun NetSocket.listenMessages(
     use {
         do {
             if (isClosed) break
-            val message = readTextFromInputStream(it)
-            block(message)
+            val message = readLineFromInputStream(it)
+            when (message) { is String -> block(message) }
         } while (message != stopMessage)
     }
 }
 
-private suspend fun readTextFromInputStream(socket: NetSocket): String = withContext(Dispatchers.IO) {
-    socket.getInputStream().reader().use { it.readLines().first() }
+internal suspend fun readLineFromInputStream(socket: NetSocket): String? = withContext(Dispatchers.IO) {
+    val inputStream = try { socket.getInputStream() } catch (ex: Exception) { return@withContext null }
+    inputStream.bufferedReader().readLine()?.trim()
 }
 
-suspend fun NetSocket.writeTextToOutputStream(block: suspend () -> String) = withContext(Dispatchers.IO) {
-    getOutputStream().writer().use{ it.write(block()) }
+internal suspend fun NetSocket.writeTextToOutputStream(block: suspend () -> String) = withContext(Dispatchers.IO) {
+    try {
+        getOutputStream().writer().run {
+            write("${block()}\n")
+            flush()
+        }
+    } catch (ex: SocketException) {
+        this@writeTextToOutputStream.close()
+    }
 }
